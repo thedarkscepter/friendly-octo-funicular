7745dedd8781418fee9f4a8c22f5597cb9db6aec
thedarkscepter <60280897+thedarkscepter@users.noreply.github.com>
2020-12-24 16:33:01 +0530

    Add files via upload

---
+++ 91/gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+  sass = require('gulp-ruby-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  cssnano = require('gulp-cssnano'),
+  jshint = require('gulp-jshint'),
+  uglify = require('gulp-uglify'),
+  imagemin = require('gulp-imagemin'),
+  rename = require('gulp-rename'),
+  concat = require('gulp-concat'),
+  notify = require('gulp-notify'),
+  cache = require('gulp-cache'),
+  livereload = require('gulp-livereload'),
+  del = require('del');
+
+gulp.task('default', function () {
+  // place code for your default task here
+});
